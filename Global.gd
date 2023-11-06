fee2a809238d2e43fccb43c9e15d859dd533f0f0
Raideno6 <116860736+Raideno6@users.noreply.github.com>
2023-11-06 14:14:32 -0500

    Added in settings

--- Global.gd
+++ Global.gd
@@ -1,3 +1,7 @@
 extends Node
 
 var screenResolution = "1920, 1080"
+
+var width = ProjectSettings.get_setting("display/window/size/viewport_width")
+
+var height = ProjectSettings.get_setting("display/window/size/viewport_height")
--- Main Menu.gd
+++ Main Menu.gd
@@ -1,29 +1,54 @@
 extends Control
 
-var width = 1152
+var originalWidth = ProjectSettings.get_setting("display/window/size/viewport_width")
 
-var height = 648
-
-var newWidth = 1152
-
-var newHeight = 648
+var originalHeight = ProjectSettings.get_setting("display/window/size/viewport_height")
 
 var oldWidth = 1152
 
 var oldHeight = 648
 
 var windowSize
+
+@onready var heightBox = $SettingsMenu/Settings/ResolutionOptions/HeightBox
+
+@onready var widthBox = $SettingsMenu/Settings/ResolutionOptions/WidthBox
+
+@onready var warningPrompt = $SettingsMenu/Settings/WarningPrompt
+
+@onready var settingsScreen = $SettingsMenu/Settings
+
+@onready var resetButton = $SettingsMenu/Settings/ResolutionOptions/ResetBtn
+
+@onready var timeRemaining = $SettingsMenu/Settings/WarningPrompt/CountdownPrompt/Time
+
+@onready var warningPromptTimer = $SettingsMenu/Settings/WarningPrompt/PromptTimer
+
+@onready var mainScreen = $MainMenu/Main
+
+@onready var resolutionOptions = $SettingsMenu/Settings/ResolutionOptions
+
+@onready var cannotChangeLabel = $SettingsMenu/Settings/CannotChange
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MainMenu/Main.show()
-	$SettingsMenu/Settings.hide()
-	$SettingsMenu/Settings/WarningPrompt.hide()
-	print()
-	DisplayServer.window_set_size(Vector2i(width, height))
+	mainScreen.show()
+	cannotChangeLabel.hide()
+	settingsScreen.hide()
+	warningPrompt.hide()
+	resetButton.hide()
+	widthBox.set_value_no_signal(Global.width)
+	heightBox.set_value_no_signal(Global.height)
+	if !(Global.width == originalWidth) or !(Global.height == originalHeight):
+			resetButton.show()
+	else:
+		resetButton.hide()
+
+	
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	windowSize = DisplayServer.window_get_size()
+	update_countdown()
 
 func _on_play_btn_pressed():
 	get_tree().change_scene_to_file("res://start_level.tscn")
@@ -31,44 +56,85 @@ func _on_play_btn_pressed():
 
 
 func _on_settings_btn_pressed():
-	$MainMenu/Main.hide()
-	$SettingsMenu/Settings.show()
+	mainScreen.hide()
+	settingsScreen.show()
 
 
 func _on_back_button_pressed():
-	$MainMenu/Main.show()
-	$SettingsMenu/Settings.hide()
-	$SettingsMenu/Settings/WarningPrompt.hide()
+	mainScreen.show()
+	settingsScreen.hide()
+	warningPrompt.hide()
 
 
 func _on_apply_btn_pressed():
 	
-	if !(newWidth == width) or !(newHeight == height):
-		$SettingsMenu/Settings/WarningPrompt.show()
+	if !(Global.width == oldWidth) or !(Global.height == oldHeight):
+		change_screensize()
+		warningPrompt.show()
+		warningPromptTimer.start()
 
 
 func _on_width_box_value_changed(value):
-	newWidth = value
-
+	Global.width = value
+	resetButton.show()
 
 func _on_height_box_value_changed(value):
-	newHeight = value
-
+	Global.height = value
+	resetButton.show()
 
 
 func _on_revert_btn_pressed():
-	newWidth = width
-	newHeight = height
-	$SettingsMenu/Settings/WidthBox.set_value_no_signal(newWidth)
-	$SettingsMenu/Settings/HeightBox.set_value_no_signal(newHeight)
-	$SettingsMenu/Settings/WarningPrompt.hide()
+	revert_screensize()
 
 
 func _on_apply_change_pressed():
-	width = newWidth
-	height = newHeight
-	DisplayServer.window_set_size(Vector2i(width, height))
-	$SettingsMenu/Settings/WarningPrompt.hide()
+	warningPrompt.hide()
+	warningPromptTimer.stop()
+	oldHeight = Global.height
+	oldWidth = Global.width
 
 func revert_screensize():
-	
+	Global.width = oldWidth
+	Global.height = oldHeight
+	widthBox.set_value_no_signal(oldWidth)
+	heightBox.set_value_no_signal(oldHeight)
+	DisplayServer.window_set_size(Vector2i(oldWidth, oldHeight))
+	warningPrompt.hide()
+
+func change_screensize():
+	DisplayServer.window_set_size(Vector2i(Global.width, Global.height))
+	warningPrompt.hide()
+
+
+func _on_reset_btn_pressed():
+	DisplayServer.window_set_size(Vector2i(originalWidth, originalHeight))
+	widthBox.set_value_no_signal(originalWidth)
+	heightBox.set_value_no_signal(originalHeight)
+	resetButton.hide()
+
+func update_countdown():
+	var rawTimeRemaining = str(warningPromptTimer.time_left)
+	var roundedTimeRemaining = int(rawTimeRemaining)
+	timeRemaining.text = str(roundedTimeRemaining)
+
+
+func _on_prompt_timer_timeout():
+	revert_screensize()
+
+
+
+func _on_vsync_btn_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+
+func _on_full_screen_btn_toggled(button_pressed):
+	if button_pressed == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		resolutionOptions.hide()
+		cannotChangeLabel.show()
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		resolutionOptions.show()
+		cannotChangeLabel.hide()
